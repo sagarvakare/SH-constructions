af2cedb22e6cfd98c7ea5f774e6a6c2b2035b08f
sagarvakare <vakaresagar@gmail.com>
2025-12-11 16:23:31 +0530

    Day 1: Setup complete

---
+++ backend/backend/src/main/java/com/jrconstructions/backend/JrConstructionsApplication.java
@@ -0,0 +1,12 @@
+package com.jrconstructions.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrConstructionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrConstructionsApplication.class, args);
+	}
+}
